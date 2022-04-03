63059c4733abc8840f2744ff36cae9af4e992d1c
MahmoudTareek <79456883+MahmoudTareek@users.noreply.github.com>
2022-04-03 03:19:40 +0200

    Update adduser.php

--- app/views/pages/adduser.php
+++ app/views/pages/adduser.php
@@ -5,7 +5,6 @@ class AddUser extends view
   public function output()
   {
     $title = $this->model->title;
-    $data = $this->model->data;
 
     require APPROOT . '/views/inc/header.php';
     $text = <<<EOT
@@ -17,10 +16,9 @@ class AddUser extends view
   padding: 40px 30px 30px 30px;
   background-color: #ecf0f3;
   border-radius: 15px;
-  box-shadow: 13px 13px 20px #cbced1, -13px -13px 20px #fff;    
 }
 body {
-background-image: url("images/imgg.jpg");
+background-image: url("http://localhost/mvc/public/images/imgg.jpg");
 background-size: cover;
 }
 .button3 {
@@ -41,8 +39,36 @@ color: white;
 border: none;
 margin-top: 40px;
 }
-
 </style>
+
+
+<script>
+function adduservalidation(){
+
+  var passw = document.getElementById("Password").value;
+  var passw2 = document.getElementById("Password2").value;
+  var mail = document.getElementById("Email").value;
+
+  if(passw.length < 8){
+    alert("Password length must be atleast 8 characters");
+    return false;
+    }
+
+  if(passw2.length < 8){
+    alert("Re-Password length must be atleast 8 characters");
+    return false;
+    }
+
+    if(passw != passw2){
+      alert("Re-Password is not correct!")
+      return false;
+    }
+    else if (!strpos(mail, '@') !== false) {
+      alert("Email should contain @example.com");
+      return false;
+    }
+}
+</script>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -52,34 +78,38 @@ margin-top: 40px;
 
 
 <div class="container">
-  <div class="wrapper">
+<div class="wrapper">
 <center><h2>-Add User-</h2></center>
-<form>
-  <div class="form-group">
-    <label for="text">MAC Address:</label>
-    <input type="text" class="form-control" id="MacAddress" placeholder="Enter the Mac Address">
-  </div>
-  <div class="form-group">
-    <label for="text">First Name:</label>
-    <input type="text" class="form-control" id="FirstName" placeholder="Enter the first name">
-  </div>
-  <div class="form-group">
-    <label for="text">Last Name:</label>
-    <input type="text" class="form-control" id="LastName" placeholder="Enter the last name">
-  </div>
-  <div class="form-group">
-    <label for="email">Email:</label>
-    <input type="email" class="form-control" id="Email" placeholder="Enter the email">
-  </div>
-  <div class="form-group">
+<form method="post" action = "">
+<div class="form-group">
+<label for="text">First Name:</label>
+<input type="text" class="form-control" id="FirstName" placeholder="Enter the first name">
+</div>
+<div class="form-group">
+<label for="text">Last Name:</label>
+<input type="text" class="form-control" id="LastName" placeholder="Enter the last name">
+</div>
+<div class="form-group">
+<label for="email">Email:</label>
+<input type="email" class="form-control" id="Email" placeholder="Enter the email">
+</div>
+<div class="form-group">
+  <label for="text">MAC Address:</label>
+  <input type="text" class="form-control" id="MacAddress" placeholder="Enter the Mac Address">
+</div>
+<div class="form-group">
     <label for="Password">Password:</label>
     <input type="password" class="form-control" id="Password" placeholder="Enter the password">
   </div>
   <div class="form-group">
-    <label for="pwd">First Name:</label>
-    <input type="password" class="form-control" id="pwd" placeholder="Enter the first name">
+    <label for="Password">Re-Password:</label>
+    <input type="password" class="form-control" id="Password2" placeholder="Enter the Re-password">
   </div>
-  <button type="submit" class=" button2">Add</button>
+  <div class="form-group">
+  <label for="Title">Title:</label>
+  <input type="text" class="form-control" id="Title" placeholder="Enter the Title">
+</div>
+  <button type="submit" onclick="adduservalidation()" class=" button2">Add</button>
 </form> 
 </div>
 </div>
