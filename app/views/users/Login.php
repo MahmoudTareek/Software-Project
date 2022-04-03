114bb7e7fb3f2bb1b64165099e7373b064ea8903
nesmatamer19 <96889917+nesmatamer19@users.noreply.github.com>
2022-04-03 03:21:11 +0200

    Update Login.php

--- app/views/users/Login.php
+++ app/views/users/Login.php
@@ -24,30 +24,18 @@ EOT;
   private function printForm()
   {
     $action = URLROOT . 'users/login';
-    $registerUrl = URLROOT . 'users/register';
-    // $this->printEmail();
-    // $this->printPassword();
+
 
     $text = <<<EOT
-    
-<style>
+    <link rel="stylesheet" href="http://localhost/MVC/public/css/style.css">
+   <style>
 
- 
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap');
-
-
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-   font-family: 'Poppins', sans-serif
-}
-
-body {
+ body {
   
- background-image: url("http://localhost/mvc/public/images/imgg.jpg");
+ background-image: url("http://localhost/mvc/public/images/360_F_322690073_C3HGQDDRUmwHrDgf28PIRtXWFeWyBaLD.jpg");
  background-size: cover;
 }
+
 
 .wrapper {
     max-width: 350px;
@@ -56,87 +44,36 @@ body {
     padding: 40px 30px 30px 30px;
     background-color: #ecf0f3;
     border-radius: 15px;
-    box-shadow: 13px 13px 20px #cbced1, -13px -13px 20px #fff
+
     
 }
 
-.logo {
-    width: 80px;
-    margin-bottom: 30px;
-  margin-left:100px;
-  
-}
+<script >
+function signinvalidation(){
 
-.logo img {
-    width: 115%;
-    height: 90px;
-    object-fit: cover;
-    border-radius: 50%;
-    box-shadow: 0px 0px 3px #5f5f5f, 0px 0px 0px 5px #ecf0f3, 8px 8px 15px #a7aaa7, -8px -8px 15px #fff
-  
-}
+  var passw = document.getElementById("password").value;
+  var mail = document.getElementById("email").value;
 
-.wrapper .name {
-    font-weight: 600;
-    font-size: 18px;
-    letter-spacing: 1.3px;
-    padding-left: 10px;
-    color: #555
-  margin-top: 30px;
-}
+  if(passw.length < 8){
+    alert("Password length must be atleast 8 characters");
+    return false;
+    }
 
-.wrapper .form-field input {
-    width: 100%;
-    display: block;
-    border: none;
-    outline: none;
-    background: none;
-    font-size: 17px;
-    color: #666;
-    padding: 10px 15px 10px 10px
+    else if (!strpos(mail, '@') !== false) {
+      alert("Email should contain @example.com");
+      return false;
+    }
 }
+</script>
 
-.wrapper .form-field {
-    padding-left: 10px;
-    margin-bottom: 20px;
-    border-radius: 20px;
-    box-shadow: inset 8px 8px 8px #cbced1, inset -8px -8px 8px #fff
-}
-
-.wrapper .btn {
-    box-shadow: none;
-    width: 100%;
-    height: 40px;
-    background-color: lightskyblue;
-    color: #fff;
-    border-radius: 25px;
-    box-shadow: 3px 3px 3px #b1b1b1, -3px -3px 3px #fff;
-    letter-spacing: 1.3px
-}
-
-.wrapper .btn:hover {
-    background-color: lightblue;
-}
-
-.wrapper a {
-    text-decoration: none;
-    font-size: 0.8rem;
-    color: black;
-}
-
-.wrapper a:hover {
-    color: lightblue;
-}
-
-
-}
 </style>
 <div class="wrapper">
    <div class="logo"> <img src="http://localhost/mvc/public/images/logo.jpg" alt=""> </div>
    <center> <div class="text-center mt-4 name"> Al-Wastani Company</div> </center>
-   <form class="p-3 mt-3">
-       <div class="form-field d-flex align-items-center"> <span class="far fa-user"></span> <input type="text" name="userName" id="userName" placeholder="Email"> </div>
-       <div class="form-field d-flex align-items-center"> <span class="fas fa-key"></span> <input type="password" name="password" id="pwd" placeholder="Password"> </div> <button class="btn mt-3">Login</button>
+   <form class="p-3 mt-3" method="POST">
+       <div class="form-field d-flex align-items-center"> <input type="text" name="email" id="email" placeholder="Email"> </div>
+       <div class="form-field d-flex align-items-center"> <input type="password" name="password" id="password" placeholder="Password"> </div>
+       <button type="submit" onclick="signinvalidation()" class="btn mt-3">Login</button>
    </form>
    <div class="text-center fs-6"> <a href="#">Forget password?</a> </div>
 </div>
