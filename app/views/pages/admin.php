bb0de0a5d39c9f1f146f6855a9fdf72ef5bf50bb
nourAhmed123 <96887154+nourAhmed123@users.noreply.github.com>
2022-06-01 22:52:57 +0200

    Update admin.php

--- app/views/pages/admin.php
+++ app/views/pages/admin.php
@@ -19,24 +19,19 @@ class admin extends view
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 <style>
-
 table {
   border-collapse: collapse;
   width: 100%;
 }
-
 th, td {
   text-align: left;
   padding: 8px;
 }
-
 tr:nth-child(even){background-color: #f2f2f2}
-
 th {
   background-color: lightblue;
   color: white;
 }
-
 .wrapper {
     max-width: 1200px;
     min-height: 500px;
@@ -47,74 +42,47 @@ th {
     box-shadow: 5px 5px 5px 5px lightgrey;
     
 }
-
 </style>
+<?php
+class Admin extends view
+{
+
+  public function output()
+  {
+   
+
+    require APPROOT . '/views/inc/header.php';
+    $employees=$this->model->employee();
+
+?>
+
+<div class="container">
+          <div class="wrapper">
+            
+
+            <?php $str= "<table width=100%>
+                  <thead>
+                      <tr>
+                          <th>MACAddress</th>
+                          <th>Name</th>
+                          <th>Title</th>
+                          <th>Phone</th>
+                          <th>Action</th>
+
+                      </tr>
+                  </thead>";
+
+                        foreach($employees as $x){
+                        
+                          $str.="<tr><td>".$x->MAC."</td><td>".$x->name."</td><td>".$x->Title."</td><td>".$x->phone."</td><td><button style= 'background-color: transparent; border:none;'><a href='http://localhost/mvc/public/pages/edit' style ='color: #ffc107';class='edit' title='Edit' data-toggle='tooltip'><i class='material-icons'>&#xE254;</i></a></button><button style= 'background-color: transparent; border:none;''><a style ='color: red;'' class='delete' title='Delete' data-toggle='tooltip'><i class='material-icons'>&#xE872;</i></a> </center></button></td></tr>";
 
 
+                            }
 
-<body>
-<div class="wrapper">
-  <div><p style="font-size: 30px;">Employee <b>Details</b></p></div>
-<table>
-  <tr>
-    <th>Full Name</th>
-     <th>MAC Address</th>  
-     <th>Email</th>
-    <th>Title</th>
-    <th><center>Actions</center></th>
+    
+          $str.="</table>";
 
-  </tr>
-  <tr>
-    <td>Nour</td>
-    <td>43553545</td>
-    <td>No@gmail.com</td>
-    <td>IT Manager</td>
-    <td><center>
-    <button style= "background-color: transparent; border:none;"><a style ="color: #ffc107";class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a></button>
-    <button style= "background-color: transparent; border:none;"><a style ="color: red;" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a></button>
-    </td></center>
-  </tr>
-  <tr>
-    <td>Nesma</td>
-    <td>23346554</td>
-    <td>n@gmail.com</td>
-    <td>Back End developer</td>
-    <td><center>
-    <button style= "background-color: transparent; border:none;"><a style ="color: #ffc107";class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a></button>
-    <button style= "background-color: transparent; border:none;"><a style ="color: red;" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a></button>
-    </td></center>
-  </tr>
-  <tr>
-     <td>Yasmine</td>
-    <td>7864435</td>
-    <td>p@gmail.com</td>
-    <td>Front End Developer</td>
-    <td><center> 
-      <button style= "background-color: transparent; border:none;"><a style ="color: #ffc107";class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a></button>
-       <button style= "background-color: transparent; border:none;"><a style ="color: red;" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a> </center></td> </button>
-  </tr>
-  <tr>
-     <td>Mahmoud</td>
-    <td>53465354</td>
-    <td>p@gmail.com</td>
-    <td>web designer</td>
-    <td><center>
-                           
-    <button style= "background-color: transparent; border:none;"><a style ="color: #ffc107"; class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a></button>
-    <button style= "background-color: transparent; border:none;"><a style ="color: red;" class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a></button>
-    </td></center>
-</tr>
-
-</table>
-
-
-</div>
-
-</body>
-</html>
-
-EOT;
-    echo $text;
+    echo $str;
     require APPROOT . '/views/inc/footer.php';
   }
 }
