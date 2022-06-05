b1eda134d9435725d8aec452aceeb4d38fdb5c6e
yasminemsalah <101893753+yasminemsalah@users.noreply.github.com>
2022-06-05 21:21:06 +0200

    Update permission.php

--- app/views/pages/permission.php
+++ app/views/pages/permission.php
@@ -4,9 +4,7 @@ class Permission extends view
 
   public function output()
   {
-    $title = $this->model->title;
-    $data = $this->model->data;
-
+$action=URLROOT.'pages/permission';
     require APPROOT . '/views/inc/header.php';
     $text = <<<EOT
    
@@ -25,8 +23,7 @@ class Permission extends view
      
  }
 </style>
-<form>
-
+<form action="$action"method="post">
   <div class="wrapper">
      <h2><b>Leave Application Form</b></h2>
      <h6>Please fillout leave application to notify  your managers.</h6>
@@ -34,80 +31,48 @@ class Permission extends view
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
     <div class="alert alert-warning">
     <strong>Warning!</strong> Number of excuses Allowed: 4 hours per month <br><br>Number of Annual leaves Allowed : 21 Days <br><br> Incident Days : 7 Days
     </div>
-
     <h4>Duration of leave:</h4>
-
   <div class="form-row">
     <div class="form-group col-md-6">
-
       <label for="inputEmail4">From</label>
-      <input type="date" class="form-control" placeholder="26 Mar 2022, 10:00 PM">
-      <input type="time" class="form-control" placeholder="26 Mar 2022, 11:00 PM">
-
-
-
-
-
+      <input type="date" class="form-control" placeholder="26 Mar 2022, 10:00 PM" name="fromDate">
+      <input type="time" class="form-control" placeholder="26 Mar 2022, 11:00 PM" name="fromTime">
     </div>
     <div class="form-group col-md-6">
       <label for="inputPassword4">To</label>
-      <input type="date" class="form-control" placeholder="26 Mar 2022, 11:00 PM">
-      <input type="time" class="form-control" placeholder="26 Mar 2022, 11:00 PM">
+      <input type="date" class="form-control" placeholder="26 Mar 2022, 11:00 PM" name="toDate">
+      <input type="time" class="form-control" placeholder="26 Mar 2022, 11:00 PM" name="toTime">
     </div>
   </div>
-
   <h6>Please ensure your email id is autofilled, if not please login again:</h6>
-
   <div class="form-group">
     <label for="inputAddress">Email</label>
-    <input type="text" class="form-control"  placeholder="enter your email"><br><br>
-
+    <input type="text" class="form-control"  placeholder="enter your email" name="email"><br><br>
      <div class="form-group">
       <label for="inputState">Type</label>
-      <select id="inputState" class="form-control">
+      <select id="inputState" class="form-control" name="type">
         <option selected>Choose...</option>
         <option>Excuse</option>
         <option>Day off</option>
         <option>Mission</option>
       </select>
     </div>
-
-
-
-   <div class="form-group">
-      <label for="inputState">Reasons for Leave</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>Family emergency</option>
-        <option>Not feeling well</option>
-        <option>Mission to the company</option>
-      </select>
     </div>
-
     <div class="form-group">
-    <label  for="inputAddress">Any Additional Notes:</label>
-    <input style="height: 70px;" type="text" class="form-control"  placeholder="comment"><br><br>
-
+    <label  for="inputAddress">Reasons for Leave:</label>
+    <input style="height: 70px;" type="text" class="form-control"  placeholder="comment" name="reason"><br><br>
     <div class="form-group">
-       <input type="checkbox"  name="name1" value="name">
-  <label for="name1">Confirm leave application and cancel all classes during the period.</label><br><br>
-
-  <input style="background-color: blue; color: white;" type="submit" value="Submit"><br><br>
-
+       
+  <input style="background-color: blue; color: white;" type="submit" value="Submit" name="button" ><br><br>
     <div class="alert alert-warning">
     <strong>Warning!</strong> If you exceed the number of leaves available penalty will be Added.
-
-
   </div>
  <div class="alert alert-info">
     <strong>Info!</strong> Missions to the company are not counted from your available excuses.
-
   </div>
-
 </form>
 EOT;
     echo $text;
