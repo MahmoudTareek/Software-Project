d9f41aa8202bbe51888c6467643c12e2c454c855
MahmoudTareek <79456883+MahmoudTareek@users.noreply.github.com>
2022-06-08 02:58:25 +0200

    Update AttendanceModel.php

--- app/models/AttendanceModel.php
+++ app/models/AttendanceModel.php
@@ -14,8 +14,8 @@ class AttendanceModel extends model
     public function __construct()
     {
         parent::__construct();
-        $this->checkin = date("Y-m-d h:i:s a");
-        $this->checkout = date("Y-m-d h:i:s a");
+        $this->checkin = date("h:i:s a");
+        $this->checkout = date("h:i:s a");
         $this->datee = date("Y-m-d");
         $this->id = $_SESSION['user_id'];
         $this->hourss = " ";
@@ -25,7 +25,7 @@ class AttendanceModel extends model
 
     public function Checkin()
     {
-        $this->dbh->query("INSERT INTO timesheet (id,MAC,Date1,Checkin) VALUES (:ID, :Mac, :Datee, :Checkin)");
+        $this->dbh->query("INSERT INTO timesheet (id,MAC,Date1,Checkin,Showed) VALUES (:ID, :Mac, :Datee, :Checkin, '1')");
         $this->dbh->bind(':ID', $this->id);
         $this->dbh->bind(':Mac', $this->mac);
         $this->dbh->bind(':Checkin', $this->checkin);
@@ -34,21 +34,36 @@ class AttendanceModel extends model
     }
     public function Checkout()
     {
-        $this->dbh->query("UPDATE timesheet set Checkout = :Checkout WHERE id = :ID AND Date1 = :Datee");
+        $this->dbh->query("UPDATE timesheet set Checkout = :Checkout WHERE id = :ID AND Date1 = :Datee AND Showed = '1'");
         $this->dbh->bind(':ID', $this->id);
         $this->dbh->bind(':Datee', $this->datee);
         $this->dbh->bind(':Checkout', $this->checkout);
         return $this->dbh->execute();
+
     }
     public function WorkingHours()
     {
-        $this->dbh->query("SELECT Checkin, Checkout from timesheet WHERE id = :ID");
+        $this->dbh->query("SELECT * from timesheet WHERE id = :ID AND Date1 = :Datee AND Showed = '1'");
         $this->dbh->bind(':ID', $this->id);
+        $this->dbh->bind(':Datee', $this->datee);
         $record = $this->dbh->single();
-        $this->hourss = ((strtotime($record->Checkout) - strtotime ($record->Checkin))/3600);
-        $this->dbh->query("INSERT INTO timesheet Hours1 VALUES (:hourss) WHERE id = :ID");
-        $this->dbh->bind(':ID', $this->id);
+        // $this->hourss = abs(strtotime($record->Checkout) - strtotime($record->Checkin))/60/60;
+        $this->hourss = abs(strtotime($record->Checkout) - strtotime($record->Checkin));
+        $this->dbh->query("UPDATE timesheet set Hours1 = :hourss WHERE id = :ID AND Date1 = :Datee AND Showed = '1'");
         $this->dbh->bind(':hourss', $this->hourss);
+        $this->dbh->bind(':ID', $this->id);
+        $this->dbh->bind(':Datee', $this->datee);
         return $this->dbh->execute();
     }
+    public function Showed(){
+        $this->dbh->query("UPDATE timesheet set Showed = 0");
+        return $this->dbh->execute();
+    }
+    public function Report()
+    {
+        $this->dbh->query("SELECT * from timesheet WHERE id = :ID");
+        $this->dbh->bind(':ID', $this->id);
+        $record = $this->dbh->resultSet();
+        return $record;
+}
 }
